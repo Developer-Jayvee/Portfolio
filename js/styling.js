36e0193ce6e15464db8e4cc37de31df856d9b5b4
Jayvee Programmer <jayvee.github0703@gmail.com>
2026-01-10 17:27:10 +0800

    Make Portfolio Responsive (#6)
    Merging responsive/Portfolio.2 to main

--- js/styling.js
+++ js/styling.js
@@ -1,19 +1,33 @@
-    const homeElement = document.getElementById('home');
-    
-    
-    
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show');
-            } else {
-                entry.target.classList.remove('show');
-            }
-        });
-    }, {
-        threshold: 0.3, // Trigger when 30% of element is visible
-        rootMargin: '0px 0px -50px 0px' // Trigger 50px before element reaches viewport
+const homeElement = document.getElementById('home');
+const menu = document.getElementById('menu');
+const navigation = document.querySelector('.navigation')
+const main  = document.querySelector('main')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
     });
-    
-    observer.observe(homeElement);
+}, {
+    threshold: 0.3, // Trigger when 30% of element is visible
+    rootMargin: '0px 0px -50px 0px' // Trigger 50px before element reaches viewport
+});
+
+observer.observe(homeElement);
+
+menu.addEventListener('click', function(e){
+    let navHeight = navigation.clientHeight;
+  
+    navigation.style.height = navHeight === 80 ? "260px" : "80px";
+});
+
+main.addEventListener('click',function() {
+     let navHeight = navigation.clientHeight;
+    if(navHeight !== 80){
+        navigation.style.height = "80px";
+    }
+});
