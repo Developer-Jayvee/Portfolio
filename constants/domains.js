f8ae674eb2841cbcd200275b2cdb9323941ac533
Jayvee Programmer <jayvee.github0703@gmail.com>
2026-01-12 23:46:31 +0800

    feature/portfolio.5 to main (#10)
    * Change url
    
    * Commented toastr

---
+++ constants/domains.js
@@ -0,0 +1,2 @@
+
+export const SEND_API = "https://portfolio-be-production-778b.up.railway.app";
--- js/services/SendEmailService.js
+++ js/services/SendEmailService.js
@@ -1,21 +1,37 @@
+import { SEND_API } from "../../constants/domains";
 const emailForm = document.getElementById('emailForm');
-const sendEmailBtn = document.getElementById('sendEmailBtn');
-async function sendEmail(form_data) {
-    const { data } = await axios.post('http://localhost:3000/api/send-email',form_data);
-    return data;
-}
+let sendEmailBtn = document.getElementById('sendEmailBtn');
 
+
+async function sendEmail(form_data) {
+    sendEmailBtn.setAttribute('disabled',true);
+    sendEmailBtn.innerText = 'Loading...';
+    sendEmailBtn.classList.add('not-allowed');
+    try {
+        const { data } = await axios.post(SEND_API + '/api/send-mail',form_data);
+        if(data){
+            sendEmailBtn.setAttribute('disabled',false);
+        }
+        return data;
+    } catch (error) {
+        console.log(`Error occured in ${error}`);
+    }finally{
+        resetFields();
+    }
+}
+function resetFields(){
+    sendEmailBtn.removeAttribute('disabled');
+    sendEmailBtn.classList.remove('not-allowed');
+    sendEmailBtn.innerText = 'Send Email';
+    document.getElementById('email').value = "";
+    document.getElementById('message').value = "";
+}
 emailForm.addEventListener('submit', async function(e){
     e.preventDefault();
-
-    
     const form_data  = {
         email:document.getElementById('email').value,
         message:document.getElementById('message').value
     }
-    
     await sendEmail(form_data);
-
-    
 })
 
