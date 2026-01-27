9632f4283d32d2bf36844857fa03597d6f5d37a9
Jayvee Programmer <jayvee.github0703@gmail.com>
2026-01-27 09:31:00 +0800

    Merging change/Portfolio.15 to main
    Removed overflow (#16)

--- js/services/SendEmailService.js
+++ js/services/SendEmailService.js
@@ -1,11 +1,13 @@
 
 const emailForm = document.getElementById('emailForm');
 let sendEmailBtn = document.getElementById('sendEmailBtn');
-
+let  emailField =  document.getElementById('email');
+let messageField =    document.getElementById('message');
 const SEND_API = "https://portfolio-be-production-778b.up.railway.app";
 
 async function sendEmail(form_data) {
     sendEmailBtn.setAttribute('disabled',true);
+    isSetDisabled([sendEmailBtn,emailField,messageField]);
     sendEmailBtn.innerText = 'Loading...';
     sendEmailBtn.classList.add('not-allowed');
     try {
@@ -20,18 +22,31 @@ async function sendEmail(form_data) {
         resetFields();
     }
 }
+function isSetDisabled(elements , isDIsabled = true){
+    if(Array.isArray(elements) && elements.length > 0){
+        elements.forEach(element => {
+            if(isDIsabled){
+                element.setAttribute('disabled', isDIsabled);
+            }else{
+                if(element.hasAttribute('disabled')){
+                    element.removeAttribute('disabled');
+                }
+            }
+        });
+    }
+}
 function resetFields(){
-    sendEmailBtn.removeAttribute('disabled');
+    isSetDisabled([sendEmailBtn,emailField,messageField],false);
     sendEmailBtn.classList.remove('not-allowed');
     sendEmailBtn.innerText = 'Send Email';
-    document.getElementById('email').value = "";
-    document.getElementById('message').value = "";
+    emailField.value = "";
+    messageField.value = "";
 }
 emailForm.addEventListener('submit', async function(e){
     e.preventDefault();
     const form_data  = {
-        email:document.getElementById('email').value,
-        message:document.getElementById('message').value
+        email: emailField.value,
+        message:messageField.value
     }
     await sendEmail(form_data);
 })
